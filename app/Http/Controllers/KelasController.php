47d5595e7eb4d176f1eff52ea5eee7b38ea8e0fb
Gede Darma Yuda <gedheyuda398@gmail.com>
2023-01-07 20:06:49 +0700

    first commit

--- app/Http/Controllers/KelasController.php
+++ app/Http/Controllers/KelasController.php
@@ -11,23 +11,23 @@ class KelasController extends Controller
     public function showAllKelas(){
         return response()->json(Kelas::all());
     }
-    public function showOneKelas($id_mata_kuliah){
-        return response()->json(Kelas::find($id_mata_kuliah));
+    public function showOneKelas($id){
+        return response()->json(Kelas::find($id));
     }
     public function createKelas(Request $request){
         $data = $request->all();
         $kelas = Kelas::create($data);
         return response()->json($kelas, 201);
     }
-    public function updateKelas($id_mata_kuliah, Request $request){
-        $kelas = Kelas::findOrFail($id_mata_kuliah);
+    public function updateKelas($id, Request $request){
+        $kelas = Kelas::findOrFail($id);
         $data = $request->all();
         $kelas->update($data);
         return response()->json($kelas, 200);
     }
-    public function deleteKelas($id_mata_kuliah)
+    public function deleteKelas($id)
     {
-        Kelas::findOrFail($id_mata_kuliah)->delete();
+        Kelas::findOrFail($id)->delete();
         return response('Delete Berhasil', 200);
     }
 }
